6bf517f45916010c55cffc8fde67d75d90ea95b8
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2023-10-13 09:56:46 -0500

    Create dynamic_server.mjs

---
+++ dynamic_server.mjs
@@ -0,0 +1,19 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
+
+import { default as express } from 'express';
+import { default as sqlite3 } from 'sqlite3';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const port = 8000;
+const root = path.join(__dirname, 'public');
+const template = path.join(__dirname, 'templates');
+
+let app = express();
+app.use(express.static(root));
+
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
